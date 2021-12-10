e97a0dcf95a4d46796b3abcacf33cf74080d3be4
Daniela Garcia <garcia19883@iesmarenostrum.com>
2021-12-10 19:17:01 +0100

    Ejercicio 5 casi final
    Hasta la página 61

--- MvcSoporte/Controllers/HomeController.cs
+++ MvcSoporte/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MvcSoporte.Data;
 using MvcSoporte.Models;
 using System;
 using System.Collections.Generic;
@@ -13,17 +14,30 @@ namespace MvcSoporte.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MvcSoporteContexto _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MvcSoporteContexto context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            // Busca empleado correspondiente al usuario actual. Si existe
+            // va a View y en caso contrario, va a crear el empleado.
+            string emailUsuario = User.Identity.Name;
+            Empleado empleado = _context.Empleados.Where(e => e.Email == emailUsuario)
+            .FirstOrDefault();
+            if (User.Identity.IsAuthenticated &&
+            User.IsInRole("Usuario") &&
+            empleado == null)
+            {
+                return RedirectToAction("Create", "MisDatos");
+            }
             return View();
         }
-        [Authorize(Roles = "Usuario")]
+        //[Authorize(Roles = "Usuario")]
         public IActionResult Privacy()
         {
             return View();
